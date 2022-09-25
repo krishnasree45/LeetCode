320eb25ffd913cccd6e805fd5813f1969417bdae
krishnasree45 <krishnasreegandham@gmail.com>
2022-09-25 19:10:44 -0400

    Time: 4 ms (91.48%), Space: 42 MB (98.32%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        s= s.toLowerCase();
+        for(int i =0; i< s.length(); i++){
+            if((s.charAt(i) >= 'a' && s.charAt(i) <= 'z') || (s.charAt(i) >= '0' && s.charAt(i) <= '9')){
+                sb.append(s.charAt(i));
+            }
+        }
+        int i =0; 
+        int j = sb.length() -1;
+        while(i < j){
+            if(sb.charAt(i) != sb.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
