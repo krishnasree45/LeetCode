2f3a8bf1200ce08d5fba216b4e3c730d68305bab
krishnasree45 <krishnasreegandham@gmail.com>
2023-01-02 15:32:34 -0500

    Added solution - LeetHub

---
+++ Max Sum Subarray of size K - GFG/max-sum-subarray-of-size-k.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            
+            int N=sc.nextInt();
+            int K=sc.nextInt();
+            ArrayList<Integer> Arr = new ArrayList<Integer>(N); 
+  
+            // Appending new elements at 
+            // the end of the list 
+            for (int i = 0; i < N; i++) {
+                int x = sc.nextInt();
+                Arr.add(x); 
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.maximumSumSubarray(K,Arr,N));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static long maximumSumSubarray(int K, ArrayList<Integer> Arr,int N){
+        // code here
+        long maxSumSoFar = Integer.MIN_VALUE;
+        long currSum = 0;
+        for(int i = 0; i< K; i++){
+            currSum += Arr.get(i);
+        }
+        maxSumSoFar = currSum;
+        for(int i = 1; i< N-K+1; i++){
+            currSum = currSum + Arr.get(i+K-1);
+            currSum = currSum - Arr.get(i-1);
+            // System.out.println("Value of CurrSum: " + currSum);
+            // System.out.println("Max sum so far: " + maxSumSoFar);
+            maxSumSoFar = Math.max(maxSumSoFar, currSum);
+            
+        }
+        return maxSumSoFar;
+    }
+}
